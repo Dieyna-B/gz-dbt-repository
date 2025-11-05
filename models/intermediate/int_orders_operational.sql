06eff7e28280743699fd6392b11e6ec78a0f59af
b.dieyna23@gmail.com <b.dieyna23@gmail.com>
2025-11-05 15:33:11 +0000

    add revenue

--- models/intermediate/int_orders_operational.sql
+++ models/intermediate/int_orders_operational.sql
@@ -2,7 +2,8 @@ select
     date_date,
     orders_id,
     total_qty,
+    revenue,
     sum (margin + shipping_fee - logcost - ship_cost) AS Operational_margin
 from {{ ref('int_orders_margin') }}
 join {{ ref('stg_raw__ship') }} using (orders_id)
-group by date_date, orders_id, total_qty
+group by date_date, orders_id, total_qty, revenue
