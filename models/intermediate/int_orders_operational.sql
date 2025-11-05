ccec5fe57724e524e5465f5c4a11033bcad09309
b.dieyna23@gmail.com <b.dieyna23@gmail.com>
2025-11-05 15:47:02 +0000

    finance table

--- models/intermediate/int_orders_operational.sql
+++ models/intermediate/int_orders_operational.sql
@@ -3,7 +3,8 @@ select
     orders_id,
     total_qty,
     revenue,
+    purchase_price,
     sum (margin + shipping_fee - logcost - ship_cost) AS Operational_margin
 from {{ ref('int_orders_margin') }}
 join {{ ref('stg_raw__ship') }} using (orders_id)
-group by date_date, orders_id, total_qty, revenue
+group by date_date, orders_id, total_qty, revenue, purchase_price
